cc0d892a8813b29910bf00c1cf6d191db2a481d5
Alexander <91593013+akozhevnikov-rebilly@users.noreply.github.com>
2021-12-07 09:45:17 +0400

    Fix GHA config (#3)

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "github_repository" "default" {
   name       = "tf-github-experiments"
   visibility = "public"
   vulnerability_alerts = true
+  delete_branch_on_merge = true
 }
 
 resource "github_branch" "default" {
