f96f7dc25ce63fd6904b1f111d2274c87c66a5ce
Alexander Kozhevnikov <aleksandr.kozhevnikov@rebilly.com>
2021-12-07 09:50:35 +0400

    Fix TF format

--- branch_protection.tf
+++ branch_protection.tf
@@ -1,6 +1,6 @@
 resource "github_branch_protection" "default" {
   repository_id = github_repository.default.name
-  pattern = github_branch_default.default.branch
+  pattern       = github_branch_default.default.branch
   required_status_checks {
     strict   = true
     contexts = ["Apply infrastructure changes"]
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "github_repository" "default" {
-  name       = "tf-github-experiments"
-  visibility = "public"
-  vulnerability_alerts = true
+  name                   = "tf-github-experiments"
+  visibility             = "public"
+  vulnerability_alerts   = true
   delete_branch_on_merge = true
 }
 
