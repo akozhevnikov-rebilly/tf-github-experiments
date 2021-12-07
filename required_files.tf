f1e7cf879c08d9b443b51c8f07f8abe0087c59ee
Alexander Kozhevnikov <aleksandr.kozhevnikov@rebilly.com>
2021-12-07 08:36:30 +0400

    Add workflow for test

--- required_files.tf
+++
@@ -1,8 +0,0 @@
-resource "github_repository_file" "readme" {
-  repository          = github_repository.default.name
-  branch              = github_branch.default.branch
-  content             = "## ${github_repository.default.full_name}"
-  file                = "README.md"
-  commit_message      = "Add missed README.md"
-  overwrite_on_create = false
-}
